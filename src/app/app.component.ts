a8981ed23013a3701b7ca772a7ea1150625436e5
daimingzhong <daimingzhong@gmail.com>
2018-10-10 22:48:30 -0400

    add file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AgGridNg2 } from 'ag-grid-angular';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ag-grid-app';
+export class AppComponent implements OnInit {
+    @ViewChild('agGrid') agGrid: AgGridNg2;
+
+    title = 'app';
+
+    columnDefs = [
+        {headerName: 'Make', field: 'make', checkboxSelection: true },
+        {headerName: 'Model', field: 'model' },
+        {headerName: 'Price', field: 'price'}
+    ];
+
+    rowData: any;
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    ngOnInit() {
+        this.rowData = this.http.get('https://api.myjson.com/bins/15psn9');
+    }
+
+    getSelectedRows() {
+        const selectedNodes = this.agGrid.api.getSelectedNodes();
+        const selectedData = selectedNodes.map( node => node.data );
+        const selectedDataStringPresentation = selectedData.map( node => node.make + ' ' + node.model).join(', ');
+        alert(`Selected nodes: ${selectedDataStringPresentation}`);
+    }
 }
