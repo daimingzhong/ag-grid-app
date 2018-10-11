4f2ca1baf2925adb4ee590938ad376747f9a5a46
daimingzhong <daimingzhong@gmail.com>
2018-10-11 12:06:14 -0400

    add file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Local} from 'protractor/built/driverProviders';
 
 @Component({
     selector: 'app-root',
@@ -9,20 +10,22 @@ export class AppComponent {
     title = 'app';
 
     columnDefs = [
-        {headerName: 'Make', field: 'make' },
-        {headerName: 'Model', field: 'model' },
+        {headerName: 'Make', field: 'make'},
+        {headerName: 'Model', field: 'model'},
         {headerName: 'Price', field: 'price'}
     ];
 
     rowData = [
-        // new LocalData('Toyota', 'Celica', 35000),
-        // new LocalData('Ford', 'Celica', 32000),
-        // new LocalData('Porsche', 'Celica', 35000),
+        new LocalData('Toyota', 'Celica', 35000),
+        new LocalData('Ford', 'Celica', 32000),
+        new LocalData('Porsche', 'Celica', 35000),
 
-        LocalData.load('Toyota', 'Celica', 35000),
-        LocalData.load('Ford', 'Celica', 32000),
-        LocalData.load('Porsche', 'Celica', 35000),
+        // LocalData.load('Toyota', 'Celica', 35000),
+        // LocalData.load('Ford', 'Celica', 32000),
+        // LocalData.load('Porsche', 'Celica', 35000),
     ];
+
+
 }
 
 class LocalData{
@@ -30,22 +33,35 @@ class LocalData{
     model: string;
     price: number;
 
-    // constructor(make: string, model: string, price: number) {
-    //     this.make = make;
-    //     this.model = model;
-    //     this.price = price;
-    // }
-
-    static load (make: string, model: string, price: number) {
-        const localData = new LocalData();
-        localData.make = make;
-        localData.model = model;
-        localData.price = price;
-        return localData;
+    constructor(make: string, model: string, price: number) {
+        this.make = make;
+        this.model = model;
+        this.price = price;
     }
 
-    // load () {
-    //
-    // }
 
+    // static load (make: string, model: string, price: number) {
+    //     const localData = new LocalData();
+    //     localData.make = make;
+    //     localData.model = model;
+    //     localData.price = price;
+    //     return localData;
+    // }
 }
+
+interface LocalData {
+    make: string;
+    model: string;
+    price: number;
+    ha: () => void;
+}
+
+
+const makeLocalData = (make: string, model: string, price: number) => {
+    return {
+        make, model, price,
+    } as LocalData;
+};
+
+
+const data = makeLocalData('1', '', 1);
